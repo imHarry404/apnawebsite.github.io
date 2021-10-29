ca9bcc7ec88c0d02ddd55e8d58f934259e20a593
Hariom Kumar <hariom14may11@gmail.com>
2021-10-29 20:33:02 +0530

    changes done

--- js/demo.js
+++ js/demo.js
@@ -1,8 +1,3 @@
-/**
- * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
- */
-
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
@@ -12,17 +7,3 @@ document.addEventListener('DOMContentLoaded', function () {
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
-
-
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
